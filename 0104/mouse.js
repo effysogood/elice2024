a465257342adb2b7a5bca508053bb815e53db445
Saeyoung Choi <75560587+effysogood@users.noreply.github.com>
2024-04-30 14:11:42 -0400

    remove: 불필요 파일 삭제

--- 0104/mouse.js
+++
@@ -1,7 +0,0 @@
-const box = document.querySelector(".box");
-
-box.addEventListener("mouseover", (e) => {
-  console.log(e);
-  console.log(e.offsetX, e.offsetY);
-  box.textContent = `x pos - ${e.offsetX} - y pos - ${e.offsetY}`;
-});
